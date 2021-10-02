a1fe10ed090976d2a104e4a8fc4ae86c7698ea0b
ColinSORourke <csorourk@ucsc.edu>
2021-10-01 22:14:30 -0700

    Quick Error Fix

--- src/classes/room.js
+++ src/classes/room.js
@@ -2,8 +2,8 @@ class Room {
     constructor(scene, key, x, y){
         // Initialize Tilemap
         this.map = scene.add.tilemap(key);
-        let tileset = map.addTilesetImage('KenneySample', 'tiles')
-        let layer = map.createLayer("Tile Layer 1", tileset, x, y);
+        let tileset = this.map.addTilesetImage('KenneySample', 'tiles')
+        let layer = this.map.createLayer("Tile Layer 1", tileset, x, y);
         this.x = x
         this.y = y
     }
