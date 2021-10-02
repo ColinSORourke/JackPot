9469a8d8670377291d0c25e4a7ba855eae8f7f13
ColinSORourke <csorourk@ucsc.edu>
2021-10-01 21:47:33 -0700

    Player Gridmap Movement

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,10 +1,52 @@
 class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, key, frame, room){
         let myTile = room.getTileAtWorldXY(x,y)
-        console.log("x: " + myTile.x + " y: " + myTile.y)
         super(scene, myTile.x * 16, myTile.y * 16, key, frame)
         scene.add.existing(this)
         this.setOrigin(0,0)
 
+        this.tileX = myTile.x
+        this.tileY = myTile.y
+        this.room = room
     }
+
+    update(){
+        if (Phaser.Input.Keyboard.JustDown(keyUP)){
+            let moveTo = this.room.getTileAt(this.tileX, this.tileY - 1)
+            if (moveTo.properties["collides"]){
+                // Don't Move
+            } else {
+                this.tileY -= 1
+                this.y = this.tileY * 16
+            }
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyDOWN)){
+            let moveTo = this.room.getTileAt(this.tileX, this.tileY + 1)
+            if (moveTo.properties["collides"]){
+                // Don't Move
+            } else {
+                this.tileY += 1
+                this.y = this.tileY * 16
+            }
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            let moveTo = this.room.getTileAt(this.tileX - 1, this.tileY)
+            if (moveTo.properties["collides"]){
+                // Don't Move
+            } else {
+                this.tileX -= 1
+                this.x = this.tileX * 16
+            }
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)){
+            let moveTo = this.room.getTileAt(this.tileX + 1, this.tileY)
+            if (moveTo.properties["collides"]){
+                // Don't Move
+            } else {
+                this.tileX += 1
+                this.x = this.tileX * 16
+            }
+        }
+    }
+
 }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -6,28 +6,30 @@ class Play extends Phaser.Scene {
     create(){
         console.log("We did it!")
 
+        // Initialize Tilemap
         this.map = this.add.tilemap('roomA');
         let tileset = this.map.addTilesetImage('KenneySample', 'tiles')
         let layer = this.map.createLayer("Tile Layer 1", tileset, 0, 0);
 
+        // Create Player
         this.player = new Player(this, 37, 69, 'testPlayer', 0, this.map)
 
+        // Create Pause Button
         this.pauseButton = this.add.text(game.config.width/2, game.config.height - 25, 'PAUSE', 20).setOrigin(0.5)
-        // Give Menu Button purpose
+        // Give Pause Button purpose
         this.pauseButton.setInteractive();
         this.pauseButton.on('pointerdown', () => {
             this.pause();
         });
-    }
 
-    onBounce(body){
-        let color = new Phaser.Display.Color()
-        color.random(50)
-        body.gameObject.setTint(color.color)
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
     }
 
     update(){
-        
+        this.player.update();
     }
 
     pause() {
