29627f11f5d7f25be8dd8895213b6ec35f12b052
ColinSORourke <csorourk@ucsc.edu>
2021-10-03 20:49:38 -0700

    Destroy and replace rooms working

--- src/classes/slotGrid.js
+++ src/classes/slotGrid.js
@@ -3,6 +3,10 @@ class slotGrid {
         this.scene = scene
         this.slots = [[],[],[]]
 
+        this.startX = x
+        this.startY = y
+        this.margins = margins
+
         let j = 0
         let i = 0
         while (j < 3){
@@ -19,5 +23,38 @@ class slotGrid {
         let column = randInt(3)
         let row = randInt(3)
         return (this.slots[column][row])
+    }
+
+    deleteRooms(player){
+        let column = 0;
+        while (column < 3){
+            if (column != player.room.column){
+                let row = 0
+                while (row < 3){
+                    let currentRoom = this.slots[column][row]
+                    currentRoom.destroy()
+                    
+                    row += 1
+                }
+            }
+
+            column += 1
+        }
+    }
+
+    replaceRooms(player){
+        let column = 0;
+        while (column < 3){
+            if (column != player.room.column){
+                let row = 0
+                while (row < 3){
+                    this.slots[column].push(new Room(this.scene, 'roomA', this.startX + 7*32*column + this.margins*column, this.startY + 7*32*row + this.margins*row, this, column, row))
+                    
+                    row += 1
+                }
+            }
+
+            column += 1
+        }
     }
 }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -15,6 +15,8 @@ class Play extends Phaser.Scene {
         // List of Enemies
         this.enemies = []
 
+        this.slots.deleteRooms(this.player)
+
         // Create Pause Button
         this.pauseButton = this.add.text(game.config.width/2, game.config.height - 25, 'PAUSE', 20).setOrigin(0.5)
         // Give Pause Button purpose
@@ -44,8 +46,9 @@ class Play extends Phaser.Scene {
         this.player.update();
         
         if (Phaser.Input.Keyboard.JustUp(keySPACE)){ //spawn skeleton TEMPORARY
-            let newEnemy = new Enemy(this, 1, 1, 'testEnemy', 0, this.player.room, 'skeleton')
-            this.enemies.push(newEnemy)
+            //let newEnemy = new Enemy(this, 1, 1, 'testEnemy', 0, this.player.room, 'skeleton')
+            //this.enemies.push(newEnemy)
+            this.slots.replaceRooms(this.player);
         }
     }
 
